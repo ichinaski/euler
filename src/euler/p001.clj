52b1680fc426f26a4ca8f2dc9a1b1b758991ba91
Inigo Lopez de Heredia <inigo.lheredia@gmail.com>
2013-10-21 19:16:29 +0100

    Use zero? function

--- src/euler/p001.clj
+++ src/euler/p001.clj
@@ -3,6 +3,6 @@
 ; 1 - Multiples of 3 and 5
 ; Sum the multiples of 3 and 5 below 1000
 (defn euler-1 []
-  (reduce + (filter #(or (= 0 (mod % 3))
-                         (= 0 (mod % 5)))
+  (reduce + (filter #(or (zero? (mod % 3))
+                         (zero? (mod % 5)))
                     (range 1000))))
