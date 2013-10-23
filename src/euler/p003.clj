d2be78d3e217ca4b13fe8adaa9441eb20892df89
Inigo Lopez de Heredia <inigo.lheredia@gmail.com>
2013-10-23 14:12:29 +0100

    Add problem (official) definitions

--- src/euler/p003.clj
+++ src/euler/p003.clj
@@ -1,7 +1,11 @@
 (ns euler.p003)
 
 ; 3 - Largest Prime Factor
-; Largest prime factor of 600851475143
+; ------------------------
+; 
+; The prime factors of 13195 are 5, 7, 13 and 29.
+; What is the largest prime factor of the number 600851475143?
+
 (defn divisor? [number divisor]
   (zero? (rem number divisor)))
 
--- src/euler/p005.clj
+++ src/euler/p005.clj
@@ -1,7 +1,10 @@
 (ns euler.p005)
 
 ; 5 - Smallest Multiple
-; Least common multiple of the numbers from 1 - 20
+; ---------------------
+;
+; 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+; What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 ; Naive way of getting the LCM of 2 numbers
 (defn lcm [x y]
