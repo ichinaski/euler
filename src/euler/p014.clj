eea5786c014337ab6b8f97cf4e7e1320940f5dd4
Inigo Lopez de Heredia <inigo.lheredia@gmail.com>
2013-11-06 19:08:03 +0000

    Add problem 014

---
+++ src/euler/p014.clj
@@ -0,0 +1,36 @@
+(ns euler.p014)
+
+; Longest Collatz sequence
+; ------------------------
+;
+; The following iterative sequence is defined for the set of positive integers:
+;   n → n/2 (n is even)
+;   n → 3n + 1 (n is odd)
+; Using the rule above and starting with 13, we generate the following sequence:
+;     13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+; It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+; Which starting number, under one million, produces the longest chain?
+; NOTE: Once the chain starts the terms are allowed to go above one million.
+
+(defn next-collatz [n]
+  (if (even? n)
+    (/ n 2)
+    (inc (* 3 n))))
+
+(defn collatz-count [n results]
+  (loop [coll [n]]
+    (let [tail (last coll)]
+      (cond
+        (results tail) (+ (dec (count coll)) (results tail))
+        (= tail 1)     (count coll)
+        :else          (recur (conj coll (next-collatz tail)))))))
+
+(defn euler-14 []
+  (loop [n 1, max-n n, results {max-n 1}]
+    (if (< n 1000000)
+      (let [value (collatz-count n results)
+            new-max (if (> value (results max-n))
+                           n
+                           max-n)]
+        (recur (inc n) new-max (assoc results n value)))
+      max-n)))
